c89d634f38fcf87057d10a1de9c43efadebc3666
NazarIvankiv <nazar777321@gmail.com>
2023-03-04 09:15:00 +0100

    feat: add responsive murkup to 320px

---
+++ pages/main/script.js
@@ -0,0 +1,10 @@
+// const logo = document.querySelector('#logo')
+// let x = window.matchMedia('(max-width: 720px)')
+// const menu = document.querySelector('#menu')
+// const clone = logo.cloneNode(true)
+// const menuBotton = document.querySelector('.hamburger-menu')
+// clone.classList = 'menulogo'
+// menuBotton.addEventListener('click',Logo)
+// function Logo(){
+//     document.getElementById('menu').prepend(clone)
+// }
