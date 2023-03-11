51575006b60f74a869c098221e6bfb7158677586
NazarIvankiv <nazar777321@gmail.com>
2023-03-11 12:09:46 +0100

    feat: add script for burger menu

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,10 +1,32 @@
-// const logo = document.querySelector('#logo')
-// let x = window.matchMedia('(max-width: 720px)')
-// const menu = document.querySelector('#menu')
-// const clone = logo.cloneNode(true)
-// const menuBotton = document.querySelector('.hamburger-menu')
-// clone.classList = 'menulogo'
-// menuBotton.addEventListener('click',Logo)
-// function Logo(){
-//     document.getElementById('menu').prepend(clone)
-// }
+const menu = document.getElementById('menu')
+const menu_toggle = document.getElementById('menu__toggle')
+const menuBtn = document.getElementById('menu__btn')
+menu.addEventListener('click', MenuClose)
+menu_toggle.addEventListener('click', MenuOpen)
+menu.classList.add('close-menu')
+function MenuClose(){
+    menu.classList.remove('open-menu')
+    menu.classList.add('close-menu')
+    menu_toggle.addEventListener('click', MenuOpen)
+    menuBtn.classList.remove('menu-rotate')
+    enableScroll()
+}
+function MenuOpen(){
+    menu.classList.remove('close-menu')
+    menu.classList.add('open-menu')
+    menu_toggle.removeEventListener('click', MenuOpen)
+    menu_toggle.addEventListener('click', MenuClose)
+    menuBtn.classList.add('menu-rotate')
+    disableScroll()
+}
+
+function disableScroll() {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    window.onscroll = function() {
+    window.scrollTo(scrollLeft, scrollTop);
+    };
+    }
+    function enableScroll() {
+    window.onscroll = function() {};
+    }
